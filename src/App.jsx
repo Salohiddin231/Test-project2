4753d59cd8fa9a5e61e15e39e3e561eeb3f21f38
Salohiddin231 <davletovsalohiddin231@gmail.com>
2026-01-19 15:24:31 +0600

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,15 @@
 import "./App.scss";
 import { FaCode } from "react-icons/fa";
+import products from "./products.json";
 
 export default function App() {
-  const product = [
-    {
-      icon: FaCode,
-      title: "Программирование",
-      desc: "Обучение с упором на практику и реальные задачи",
-      info: "Срок обучения: 3 года",
-    },
-     {
-      icon: FaCode,
-      title: "Программирование",
-      desc: "Обучение с упором на практику и реальные задачи",
-      info: "Срок обучения: 3 года",
-    },
-     {
-      icon: FaCode,
-      title: "Программирование",
-      desc: "Обучение с упором на практику и реальные задачи",
-      info: "Срок обучения: 3 года",
-    },
-     {
-      icon: FaCode,
-      title: "Программирование",
-      desc: "Обучение с упором на практику и реальные задачи",
-      info: "Срок обучения: 3 года",
-    },
-     {
-      icon: FaCode,
-      title: "Программирование",
-      desc: "Обучение с упором на практику и реальные задачи",
-      info: "Срок обучения: 3 года",
-    },
-     {
-      icon: FaCode,
-      title: "Программирование",
-      desc: "Обучение с упором на практику и реальные задачи",
-      info: "Срок обучения: 3 года",
-    },
-     {
-      icon: FaCode,
-      title: "Программирование",
-      desc: "Обучение с упором на практику и реальные задачи",
-      info: "Срок обучения: 3 года",
-    },
-     {
-      icon: FaCode,
-      title: "Программирование",
-      desc: "Обучение с упором на практику и реальные задачи",
-      info: "Срок обучения: 3 года",
-    },
-     {
-      icon: FaCode,
-      title: "Программирование",
-      desc: "Обучение с упором на практику и реальные задачи",
-      info: "Срок обучения: 3 года",
-    },
-  ];
-
   return (
     <div className="app">
       <div className="app_grid">
-        {product.map((item, items) => {
-          const Icon = item.icon;
+        {products.map((item, index) => {
           return (
-            <div className="app_card" key={items}>
-              <Icon className="app_icon" />
+            <div className="app_card" key={index}>
+              <FaCode className="app_icon" />
               <h3 className="app_title">{item.title}</h3>
               <p className="app_suptitle">{item.desc}</p>
               <span className="app_text">{item.info}</span>
